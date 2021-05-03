ad3571b42e37a3073c76cc2ed6a1d7f9cf30a422
Osnyder12 <snyderowen12@gmail.com>
2021-05-03 13:44:42 -0400

    added methods table

---
+++ db/migrate/20210503174027_methods.rb
@@ -0,0 +1,12 @@
+class Methods < ActiveRecord::Migration[5.2]
+  def change
+    create_table :methods do |t|
+      t.string :twist
+      t.integer :mash_temp_value, null: false
+      t.string :mash_temp_unit, null: false
+      t.integer :mash_temp_duration, null: false
+      t.integer :fermentation_value, null: false
+      t.string :fermentation_unit, null: false
+    end
+  end
+end
