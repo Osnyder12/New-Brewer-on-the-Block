18619d31ea3ed19540d388639ca210ac2acd12e4
Osnyder12 <snyderowen12@gmail.com>
2021-05-03 15:33:54 -0400

    changes made

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,7 +1,7 @@
 class Beer < ApplicationRecord
   validates :name, :abv, :ph, :beer_volume_value, :beer_volume_unit, :boil_volume_value, :boil_volume_unit, presence: true
 
-  has_many :methods
+  has_many :mashes
   has_many :amounts
   has_many :ingredients, through: :amounts
 end
