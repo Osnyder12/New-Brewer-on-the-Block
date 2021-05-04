4fcb183a8f91f461e34750be453820778ae623e1
Osnyder12 <snyderowen12@gmail.com>
2021-05-04 16:56:14 -0400

    added review form but cannot post the actual reviw

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :beer
+
+  validates :rating, numericality: { less_than_or_equal_to: 5 }
+  validates :difficulty, numericality: { less_than_or_equal_to: 10 }
+  validates :comment, length: { maximum: 500 }, allow_blank: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_04_192917) do
+ActiveRecord::Schema.define(version: 2021_05_04_202248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 2021_05_04_192917) do
     t.bigint "ingredient_id"
     t.index ["beer_id"], name: "index_recipes_on_beer_id"
     t.index ["ingredient_id"], name: "index_recipes_on_ingredient_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating", null: false
+    t.integer "difficulty", null: false
+    t.string "comment"
+    t.bigint "coffee_shop_id", null: false
+    t.index ["coffee_shop_id"], name: "index_reviews_on_coffee_shop_id"
   end
 
   create_table "users", force: :cascade do |t|
