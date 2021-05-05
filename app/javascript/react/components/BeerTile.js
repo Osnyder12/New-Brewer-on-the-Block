aabca54e58ad51fe7ec2bdc7ebd9a0a3ac7a2c91
Osnyder12 <snyderowen12@gmail.com>
2021-05-05 19:41:27 -0400

    added index page styling

--- app/javascript/react/components/BeerTile.js
+++ app/javascript/react/components/BeerTile.js
@@ -7,6 +7,10 @@ const BeerTile = (props) => {
       <Link to={`/beers/${props.id}`}>
         <div className="column-4">
           <h3 className="beer-text">{props.name}</h3>
+          <div className="beer-specs">
+            <ul>ABV: {props.abv}%</ul>
+            <ul>IBU: {props.ibu}</ul>
+          </div>
         </div>
       </Link>
     </div>
