aedad24753ea211fc200e136cd635ea7a6927abf
Osnyder12 <snyderowen12@gmail.com>
2021-05-06 13:33:52 -0400

    delete function working

--- app/javascript/react/components/ReviewsContainer.js
+++ app/javascript/react/components/ReviewsContainer.js
@@ -4,7 +4,18 @@ import ReviewTile from "./ReviewTile";
 const ReviewsContainer = (props) => {
   if (props.reviews.length > 0) {
     const reviewArray = props.reviews.map((review) => {
-      return <ReviewTile key={review.id} review={review} />;
+      const handleClick = () => {
+        props.deleteReview(review.id);
+      };
+
+      return (
+        <ReviewTile
+          key={review.id}
+          review={review}
+          currentUser={props.currentUser}
+          handleClick={handleClick}
+        />
+      );
     });
     return <ul className="reviews">{reviewArray}</ul>;
   } else {
