bd00eb22bbd5803af808b04db0ac81270a25e822
Osnyder12 <snyderowen12@gmail.com>
2021-05-13 13:42:35 -0400

    serializer

--- app/serializers/beer_show_serializer.rb
+++ app/serializers/beer_show_serializer.rb
@@ -1,4 +1,4 @@
-class BeerShowSerializer < ActiveModel::serializer
+class BeerShowSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :abv, :image_url, :ibu, :ph, :beer_volume_value, :beer_volume_unit, :boil_volume_value, :boil_volume_unit
 
   has_many :reviews
