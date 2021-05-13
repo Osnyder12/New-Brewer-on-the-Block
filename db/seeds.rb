6a52bf361153405afd3b053ff1d1764c17ae661d
Osnyder12 <snyderowen12@gmail.com>
2021-05-13 14:11:04 -0400

    seeds notes added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 require 'faraday'
 
+
+# punkClient = PunkAPIClient.seed_recipes 
+
+
 punk_url = "https://api.punkapi.com/v2/beers"
   punk_response = Faraday.get(punk_url)
   parsed_response = JSON.parse(punk_response.body)
@@ -91,7 +95,17 @@ yeast_ingredient4.save
 parsed_response.each do |beer|
   # consider adding all mash values to the beer table to combine them into one
   saved_beer = PunkBeer.create(name: beer["name"], description: beer["description"], abv: beer["abv"], image_url: beer["image_url"], ibu: beer["ibu"], ph: beer["ph"], beer_volume_value: beer["volume"]["value"], beer_volume_unit: beer["volume"]["unit"], boil_volume_value: beer["boil_volume"]["value"], boil_volume_unit: beer["boil_volume"]["unit"], mash_temp_value: beer["method"]["mash_temp"][0]["temp"]["value"], mash_temp_unit: beer["method"]["mash_temp"][0]["temp"]["unit"], mash_temp_duration: beer["method"]["mash_temp"][0]["duration"], fermentation_value: beer["method"]["fermentation"]["temp"]["value"], fermentation_unit: beer["method"]["fermentation"]["temp"]["unit"])
+
   PunkIngredient.create(ingredient_type: "yeast", name: beer["ingredients"]["yeast"], punk_beer: saved_beer)
+
+  # beer["ingredients"]["malt"].each do |malt|
+  #   PunkIngredient.create(ingredient_type: "malt", name: malt["name"] , amount_value: malt["amount"]["value"], amount_unit: malt["amount"]["unit"], punk_beer: saved_beer)
+  # end
+
+  # saved_beer = PunkBeer.find_by(name: beer["name"])
+  # beer["ingredients"]["hops"].each do |hop|
+  #   PunkIngredient.create(ingredient_type: "hop", name: hop["name"] , amount_value: hop["amount"]["value"], amount_unit: hop["amount"]["unit"], add_time: hop["add"], punk_beer: saved_beer)
+  # end
 end
 
 parsed_response.each do |beer|
